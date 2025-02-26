9b40398927e4009323c09d130266a18e1efc8a07
timak <timak@TIMAK>
2025-02-26 16:22:15 +0300

    Добавьте файлы проекта.

---
+++ netExmaMP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace netExmaMP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
