a18a17c00002150f1d3e73e0e27f819bdf1faf34
Тимофей Кацубо <tima.kacubo@mail.ru>
2025-03-13 17:41:19 +0300

    Добавлена логика чтения медиа

--- netExmaMP/MainWindow.xaml.cs
+++ netExmaMP/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace netExmaMP
 {
@@ -16,9 +18,51 @@ namespace netExmaMP
     /// </summary>
     public partial class MainWindow : Window
     {
+        double[] speeds = new double[9];
+        public double[] Speeds { get { return speeds; } }
+        MediaPlayer player;
+        VideoDrawing videoDrawing;
+        DispatcherTimer timer = new();
+
         public MainWindow()
         {
+            SpeedCBInit();
+            this.DataContext = this;
+
             InitializeComponent();
+
+            SpeedCB.SelectedValue = 1.0;
+
+            player = new MediaPlayer();
+            player.MediaOpened += Player_MediaOpened;
+            player.MediaEnded += Player_MediaEnded;
+
+            videoDrawing = new VideoDrawing();
+            videoDrawing.Player = player;
+            videoDrawing.Rect = DrawingRect.RenderedGeometry.Bounds;
+
+            timer.Interval = TimeSpan.FromSeconds(0.5);
+            timer.Tick += timer_tick;
+
+            VolumeSlider.Value = player.Volume * 100;
+        }
+
+        private void SpeedCBInit()
+        {
+            for (int i = 0; i < 8; i++) speeds[i] = 0.5 + 0.25 * (8 - i);
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new()
+            {
+                Filter= "Media files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg|All files (*.*)|*.*"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                //!!!!
+                OpenMediaFile(new Uri(dialog.FileName, UriKind.Absolute));
+            }
         }
     }
 }
---
+++ netExmaMP/MediaPlayerRelations.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace netExmaMP
+{
+    public partial class MainWindow
+    {
+        bool? IsLooped = false;
+        bool IsShuffled = false;
+
+        double volumeData = 0.5;
+
+        private void OpenMediaFile(Uri uri)
+        {
+            player.Open(uri);
+            PausePlayTBtn.IsChecked = true;
+            TimeTB.IsReadOnly = true;
+        }
+
+        private void Player_MediaOpened(object? sender, EventArgs e)
+        {
+            TimeTB.Text = TimeSpan.FromSeconds(0).ToString(@"hh\:mm\:ss");
+            TimelaneSlider.Value = 0;
+            TimelaneSlider.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
+            MaxTimeLabel.Content = player.NaturalDuration.TimeSpan.ToString(@"hh\:mm\:ss");
+            timer.Start();
+            player.Play();
+        }
+
+        private void Player_MediaEnded(object? sender, EventArgs e)
+        {
+            switch (IsLooped)
+            {
+                case true:
+                    player.Position = TimeSpan.FromSeconds(0);
+                    break;
+            }
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            TimelaneSlider.Value = player.Position.TotalSeconds;
+            TimeTB.Text = player.Position.ToString(@"hh\:mm\:ss");
+        }
+
+        private void LoopTBtn_Click(object sender, RoutedEventArgs e)
+        {
+            IsLooped = LoopTBtn.IsChecked;
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PausePlayTBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch (PausePlayTBtn.IsChecked)
+            {
+                case true:
+                    TimeTB.IsReadOnly = true;
+                    player.Play();
+                    timer.Start();
+                    break;
+                case false:
+                    TimeTB.IsReadOnly = false;
+                    player.Pause();
+                    timer.Stop();
+                    break;
+            }
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShuffleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VolumeTBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch (VolumeTBtn.IsChecked)
+            {
+                case true:
+                    player.Volume = volumeData;
+                    break;
+                case false:
+                    volumeData = player.Volume;
+                    player.Volume = 0;
+                    break;
+            }
+        }
+
+        private void VolumeTBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            VolumePopup.IsOpen = true;
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            player.Volume = VolumeSlider.Value / 100;
+        }
+
+        private void TimeTB_TextInput(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                player.Position = TimeSpan.Parse(TimeTB.Text);
+                
+            }
+            catch (Exception ex)
+            {
+                TimeTB.Text = player.Position.ToString(@"hh\:mm\:ss");
+            }
+        }
+
+        private void TimelaneSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            player.Pause();
+            timer.Stop();
+        }
+
+        private void TimelaneSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            player.Position=TimeSpan.FromSeconds(TimelaneSlider.Value);
+            player.Play();
+            timer.Start();
+        }
+    }
+}
